b9c8e4abd0597cce8389d779f391066fb43a1097
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-14 22:44:54 +0900

    名前全角　バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :last_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :last_name_kana, presence: true
   validates :birthday, presence: true
+  with_options presence: true, format: { with:/\A[ぁ-んァ-ン一-龥]/ , message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with:/\A[ァ-ヶー－]+\z/ , message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :last_name
+  end
 end
