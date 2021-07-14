bb070b31c3a7a4ab61239b2561f53d312d750b51
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-14 17:18:18 +0900

    新規登録実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:devise sign_up, keys:[:nickname, :family_name, :last_name, :family_name_kana, :last_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :last_name, :family_name_kana, :last_name_kana, :birthday])
   end
 end
