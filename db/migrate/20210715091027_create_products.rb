f5ee398e2e29de60710bfda69b4b0c07fe063818
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-15 22:53:14 +0900

    migrate実行

--- db/migrate/20210715091027_create_products.rb
+++ db/migrate/20210715091027_create_products.rb
@@ -1,15 +1,15 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
-    create_table :products do |t|
-      t.name :string,                     null: false
-      t.description :text,                null: false
-      t.category_id :integer,             null: false
-      t.condition_id :integer,            null: false
-      t.destination_id :integer,          null: false
-      t.prefecture_id :integer,           null: false
-      t.delivery_date_id :integer,        null: false
-      t.price :integer,                   null: false
-      t.user :references,                 null: false, foreign_key:true
+    create_table :products do |t| 
+      t.string :name,                     null: false
+      t.text :description,                null: false
+      t.integer :category_id,             null: false
+      t.integer :condition_id,            null: false
+      t.integer :destination_id,          null: false
+      t.integer :prefecture_id,           null: false
+      t.integer :delivery_date_id,        null: false
+      t.integer :price,                   null: false
+      t.references :user,                 null: false, foreign_key:true
       t.timestamps
     end
   end
