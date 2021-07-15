9cb79fe6daa35b5d877eb869352ea1240d125f54
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-16 00:06:24 +0900

    priceに対するバリデーション

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,14 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :caregory_id, :condition_id, :delivery_destination_id, :prefecture_id, :delivery_date_id
+  belongs_to :caregory_id
+  belongs_to :condition_id
+  belongs_to :delivery_destination_id
+  belongs_to :prefecture_id
+  belongs_to :delivery_date_id
 
   with_options presence: true do
     validates :name
     validates :description
-    validates :price
     validates :user
     with_optins numericality: { other_than: 1 , message: "can't be blank"} do
       validates :category_id
@@ -15,4 +18,6 @@ class Product < ApplicationRecord
       validates :delivery_date_id
     end
   end
+  validates :price, presence: true, format{with: /\A[0-9]+\Z/},numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "out of setting range"}
+
 end
