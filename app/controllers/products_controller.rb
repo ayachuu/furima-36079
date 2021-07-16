f4fba53d9577aacbbf26d100fb7ed3d5590e8ac2
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-16 12:32:03 +0900

    createアクション　ルーティング、設定済み

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,19 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def product_params
+    params[:product].permit(:name, :description, :category_id, :condition_id, :delivery_destination_id, :prefecture_id, :derivery_date_id, :price).merge(user_id: current_user.id)
+  end
 end
+
