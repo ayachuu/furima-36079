def18b39865a0c54899307e13d45e19999ae193e
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-19 14:55:03 +0900

    rubocop実施

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,8 +44,6 @@ class ProductsController < ApplicationController
 
   def move_to_path
     @product = Product.find(params[:id])
-    unless user_signed_in? && current_user.id == @product.user_id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in? && current_user.id == @product.user_id
   end
 end
