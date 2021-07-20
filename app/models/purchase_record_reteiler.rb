51ba0b1eeb62f868a2b8958fded6c2c0957071ea
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-20 17:55:47 +0900

    PurchaseRecordReteilerモデル記述変更

--- app/models/purchase_record_reteiler.rb
+++ app/models/purchase_record_reteiler.rb
@@ -1,19 +1,20 @@
-class PurchaseRecordRetailer
-  include ActiveModel::module
-  attr_acceser :user_id, :product_id, :post_code, :prefecture_id, :city, :address, :build_name, :phone_number, :purchase_record_id
+class PurchaseRecordReteiler
+  include ActiveModel::Model
+  attr_accessor :user_id, :product_id, :post_code, :prefecture_id, :city, :address, :build_name, :phone_number, :purchase_record_id
   with_options presence: true do
-    validates :user_id,
-    validates :product_id,
-    validates :post_code,format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id,
-    validates :city,
-    validates :address,
-    validates :phone_number, format: {with: ^0\d{10,11}$}
+    validates :user
+    validates :product
+    validates :prefecture_id
+    validates :city
+    validates :address
     validates :purchase_record_id
   end
+  validates :post_code,presence: true,format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :phone_number, presence: true, format: {with: /\A^0\d{10,11}$\z/}
+  validates :phone_number, format: {with: /\A^0\d{10,11}$\z/}
 
   def save
    purchase_rcord = PurchaseRecord.create(user_id: use_id, product_id: product_id)
-   Retailer.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, build_name: build_name, :phone_number, :purchase_record_id)
+   Retailer.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, build_name: build_name, phone_nmber: phone_number, purchase_record_id: purchase_record_id)
   end
 end
