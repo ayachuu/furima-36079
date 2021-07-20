91f05365b958b9f5459208198854dd6baf1b7cea
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-20 16:19:07 +0900

    ルーティングの設定を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
-  resources :products, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :purchase_records, only: [:create] do
-    resources :retailers, only: :index
+  resources :products do
+    resources :purchase_records, only: [:index, :create]
   end
 end
