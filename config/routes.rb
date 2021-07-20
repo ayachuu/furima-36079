869f8e5ee608b607ae63a073533912240456d559
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-20 15:52:34 +0900

    ルーティング　ネストで作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
   resources :products, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :purchase_records, only: [:create] do
+    resources :retailers, only: :index
+  end
 end
