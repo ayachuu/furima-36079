3ea43be3139adaad103c16663ce893276c7d3577
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-21 13:46:40 +0900

    購入の挙動に合わせてコード変更

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,9 +1,25 @@
 class PurchaseRecordsController < ApplicationController
   def index
-    @purchase_record = PurchaseRecord.all
+    @purchase_record_reteiler = PurchaseRecordReteiler.new
+    @product = Product.find(params[:product_id])
   end
 
   def create
-    binding.pry
+    @product = Product.find(params[:product_id])
+    @purchase_record_reteiler = PurchaseRecordReteiler.new(purchase_record_reteiler_params)
+    if @purchase_record_reteiler.valid?
+      @purchase_record_reteiler.save
+      redirect_to root_path
+    else
+      render :index
+    end
+    
+  end
+
+  private
+
+  def purchase_record_reteiler_params
+    params.require(:purchase_record_reteiler).permit(:post_code, :prefecture_id, :city, :address, :build_name, :phone_number, :user_id, :product_id).merge(user_id: current_user.id, product_id: params[:product_id])
   end
 end
+
--- app/models/purchase_record_reteiler.rb
+++ app/models/purchase_record_reteiler.rb
@@ -1,20 +1,19 @@
 class PurchaseRecordReteiler
+
   include ActiveModel::Model
-  attr_accessor :user_id, :product_id, :post_code, :prefecture_id, :city, :address, :build_name, :phone_number, :purchase_record_id
+  attr_accessor :post_code, :prefecture_id, :city, :address, :build_name, :phone_number, :user_id, :product_id
+  
   with_options presence: true do
-    validates :user
-    validates :product
-    validates :prefecture_id
+    validates :user_id
+    validates :product_id
+    validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
     validates :address
-    validates :purchase_record_id
   end
   validates :post_code,presence: true,format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :phone_number, presence: true, format: {with: /\A^0\d{10,11}$\z/}
-  validates :phone_number, format: {with: /\A^0\d{10,11}$\z/}
-
   def save
-   purchase_rcord = PurchaseRecord.create(user_id: use_id, product_id: product_id)
-   Retailer.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, build_name: build_name, phone_nmber: phone_number, purchase_record_id: purchase_record_id)
+   purchase_record = PurchaseRecord.create(user_id: user_id, product_id: product_id)
+   Retailer.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, build_name: build_name, phone_number: phone_number, purchase_record_id: purchase_record.id)
   end
 end
