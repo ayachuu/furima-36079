4fd3d91e58d3d59687510af35821ef5742f8e9f1
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-21 14:48:06 +0900

    ログイン状態でも自分の購入ページに行けない

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,7 +1,13 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!
+
+
   def index
     @purchase_record_reteiler = PurchaseRecordReteiler.new
     @product = Product.find(params[:product_id])
+    if current_user == @product.user
+      redirect_to root_path
+    end
   end
 
   def create
@@ -21,5 +27,8 @@ class PurchaseRecordsController < ApplicationController
   def purchase_record_reteiler_params
     params.require(:purchase_record_reteiler).permit(:post_code, :prefecture_id, :city, :address, :build_name, :phone_number, :user_id, :product_id).merge(user_id: current_user.id, product_id: params[:product_id])
   end
+
+ 
+
 end
 
