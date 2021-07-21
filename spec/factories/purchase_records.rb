bb8d2785ad09a7c9ed9f2b6e2e0dc9fba0620d2e
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-21 14:34:47 +0900

    factoryBot記述

--- spec/factories/purchase_records.rb
+++ spec/factories/purchase_records.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :purchase_record do
-    
+    association :user,:product
+
   end
 end
--- spec/factories/retailers.rb
+++ spec/factories/retailers.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :retailer do
-    
+    post_code     { "111-2222" }
+    prefecture_id { 2 }
+    city          { "名古屋市" }
+    address       { "中区" }
+    build_name    { "asdf" }
+    phone_number  { "09099998888" }
+    association :purchase_record
   end
 end
