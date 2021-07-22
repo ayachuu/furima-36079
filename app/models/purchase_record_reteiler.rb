fdfc84739c2592fb963348306b35c00f842d8525
ayachuu <hahihuheho-ayachu-buu.0528@mineo.jp>
2021-07-22 18:08:24 +0900

    validatesまとめる

--- app/models/purchase_record_reteiler.rb
+++ app/models/purchase_record_reteiler.rb
@@ -9,9 +9,9 @@ class PurchaseRecordReteiler
     validates :city
     validates :address
     validates :token
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :phone_number, format: { with: /\A^0\d{10,11}$\z/ }
   end
-  validates :post_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-  validates :phone_number, presence: true, format: { with: /\A^0\d{10,11}$\z/ }
   def save
     purchase_record = PurchaseRecord.create(user_id: user_id, product_id: product_id)
     Retailer.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, build_name: build_name,
